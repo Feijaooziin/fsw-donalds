6254a42b905c5f49e57a6c2f00394ae34492344d
Feijaoozin <gonardo1234@gmail.com>
2025-06-27 08:24:33 -0300

    chore: add eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 
 const HomePage = () => {
   return (
-    <div className=" p-5 border border-red-500 rounded-xl">
+    <div className="rounded-xl border border-red-500 p-5">
       <h1 className="text-red-500">Olá Mundo!</h1>
       <Input placeholder="Oie" />
       <Button>Testando</Button>
